f39c6c32666617a4a6199a647924770fac1f0133
kuroi <joan2000oviedo@gmail.com>
2024-05-07 16:01:24 -0500

    commit initial

--- src/app/core/service/email-service.service.ts
+++ src/app/core/service/email-service.service.ts
@@ -1,13 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EmailDTO } from '../models/email-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailServiceService {
 
-  url = "https://devkuroi.up.railway.app";
+  url = "https://devkuroi.up.railway.app/send-email";
 
-  constructor() {
+  constructor(private http: HttpClient) {}
 
-   }
+  submitForm(emailDTO: EmailDTO) {
+    this.http.post<EmailDTO>(this.url, emailDTO);
+  } 
+  
 }
--- src/app/shared/contactme/contactme.component.ts
+++ src/app/shared/contactme/contactme.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { EmailDTO } from '../../core/models/email-dto';
+import { EmailServiceService } from '../../core/service/email-service.service';
 
 @Component({
   selector: 'app-contactme',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contactme.component.html',
   styleUrl: './contactme.component.css'
 })
 export class ContactmeComponent {
+
+  contactForm = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.nullValidator]),
+    email: new FormControl("", [Validators.required, Validators.email, Validators.nullValidator]),
+    message: new FormControl("", [Validators.required, Validators.nullValidator])
+  })
+
+  constructor(private emailService: EmailServiceService ) {}
+
+  emailDTO: EmailDTO = {
+    name: this.contactForm.value.name ?? "",
+    email: this.contactForm.value.email ?? "",
+    message: this.contactForm.value.message ?? ""
+  }
+
+  submitForm() {
+    this.emailService.submitForm(this.emailDTO);
+  }
 
 }
